2ac1f09f79d3e07478f74a1477f3622839c14299
Naresh Kumar <banvro07@gmail.com>
2023-08-18 17:07:26 +0530

    Python intro

---
+++ aman.py
@@ -0,0 +1,192 @@
+            # Python History :
+
+# Guido Van Rossum
+
+
+# 1980 ------  1991
+
+# 1991 ==>  python 0.9.0
+
+# 2000    -----> python 2     (2020)
+
+# 2008 ====> python 3 
+
+# python 3.11.4
+
+
+
+# Python features:
+    # simple
+    # gernal perpose language :  
+        # 1) Web devlopment  :  Django, Flask
+        # 2) MObile aap backend devlopment   :  DRF (django rest framework)
+        # 3) Desktop application   :   pyQT5
+        # 4) AI  :
+    # high level language
+        # lanagues:
+        #     high lavel language
+        #     low level language : machine labnguage :  01
+    
+# object oriented (oops)
+        # 1) inharitance
+        # 2) polymorphisum    
+        # 3) Encapsulation
+        # 4) Abstraction
+
+# Dnymiccly typed : 
+
+a = 10
+
+# int a;
+# a = 10
+
+
+    # case senstive
+        # age = 10
+
+        # Age = 20
+
+        # b = 20
+
+
+    # interpreated:
+            # compliler     vs       interpreter
+
+# screpting:
+
+
+# print("kjasdkasdbj")
+
+# print "akdkbad"
+
+
+
+# print("Hello word")
+
+# keyword : 
+    # int, float, def ........
+
+# indentifires
+
+
+# abc = 20
+
+# indenetion : 
+
+
+# int main(){
+#     sfbskdf
+#     sdfsdfsjdfbskdf
+#     sdjfskdfbskdfjdsfsd
+#     fdsfjbdsfjdkfdsf
+# }
+
+
+# def xya():
+#     print("hello")
+
+
+# comments :  use to increse code readability
+
+
+
+# 1) Single line cmment
+
+# lsdfsdfsdfss fsdjbflsd fsdjfbsdfslkfbsl
+
+
+# 2) multiline comment
+
+"""
+    kasbjfajdbkasd
+    aaskjdbkasbdkad
+    asjdbkasbdjasbdkad
+"""
+
+
+# VARIABLES? : are use to hold data
+
+# 1) Variables never start from interger or spacial symbools
+# 2) Always start from charcters or undercore
+# 3) Variable never contain any space
+# 4) keywerds never be a variable
+
+
+# age12 = 10
+
+# _varj = 20
+
+# 1a = 10
+
+# my_name = "done"
+
+# data types in python : 
+
+# 1) Interger  ==> 12,23, 34, 45, -10
+# 2) Float  => 12.9, 70.9
+# 3) String   =>   "12"
+# 4) Boolean   => True and False
+# 5) complex   => a + bj
+
+# 6) Tuple    =>  ()
+# 7) List  =>  []
+# 8) Set => {}
+# 9) Dictionry   => {key:vale}
+
+
+# function:
+
+# print() => to print someting on console
+# input()  => to get input from user
+# len()   => to get number of elemnts in data
+# type()   => to get datatype of a varible
+
+# a = "this is me data"
+# b = 100
+
+# print("i am ok print mee... : * # $", b, a)
+
+# a = "b12b"
+
+# print(len(a))
+
+# a = 2 + 5j
+
+# collection of multipal data in a single variable
+# a = 10
+# a = (12, "helo", "car", 12.8)
+
+# a = [12, "i am ok", "no", 100]
+
+# a = {12, 23, 34, "dsd"}
+
+# a = {"name" : "done", "age" : 2}
+# print(type(a))
+
+# Input function:
+
+# input()
+
+# name = input("Enter your name : ")
+
+# print("user entred : ", name)
+
+
+# tuple, list, dict, int 
+
+
+# Numbers : 
+
+# +, -, *, /, %
+
+# 10 + 30
+
+# Math 
+
+import math 
+
+a = 20
+
+print(math.sqrt(80))
+
+
---
+++ asrclass.py
@@ -0,0 +1,138 @@
+# # Python : 
+    # 1) Simple
+    # 2) Gernal perpose
+    # 3) High lavel
+    # 4) Object oriented
+    # 5) Interpreted
+    # 6) Dynmiclly types
+    # 7) case senstive or screpting language..
+
+
+# python is simple : 
+    # c, java, php, r, rubby
+
+
+# Gernal perpose : various perpose 
+    # 1) Web devlopment   => DJango, Flask
+
+    # 2) Mobile app bacekned   => DRF (Django rest framework)   APIs
+
+    # 3) AI => 
+
+
+# High lavel 
+
+# languages:
+# 1) High level languages :
+
+# 2) Low lavel languages (machine language...)
+# binary language
+# 0 1
+
+
+# Interpreted 
+
+# complier
+
+
+# interpreter
+
+
+# Object oriented programming:
+    # classes or object
+    # 1) Inharitance
+    # 2) Abstraction
+    # 3) Polymorphisum
+    # 4) Encapsulation
+
+
+
+#  Dynmiclly types :
+
+    # int a, b;
+
+    # a = 10
+
+    # a = 10.2
+
+
+# case senstive : 
+
+# a = 10
+
+# A = 20
+
+# age = 20
+
+# Age = 100
+
+
+
+# screpting language:
+
+
+
+
+# core functons
+
+# print()  => use to print something on console
+
+# input()  => to get input from user 
+
+
+# comments:
+
+# single line comments
+
+# sdjbkjdbfkad
+
+# print("kjbsdkjasd28349238427983y4h23ebwje")
+
+# data types
+
+# a = "10.2"
+
+# # type()
+# print(type(a))
+
+
+# 1) integer    => 1, 3, 232, -283
+# 2) Float   => 12.0, 100.0
+# 3) String    => "ahsdkajbsdk"
+# 4) Boolean   => True, False
+# 5) COmplex   => a + bj
+
+# 6) list
+# 7) Tuple 
+# 8) Set 
+# 9) Dectionry
+
+# a = 20
+# b = 100
+
+# print(a, b, "this is a tring")
+
+# a = 10
+
+# b = 30
+
+# # 10 + 40
+# c = a + b
+
+# print(c)
+
+# input() => to get input from user
+
+# name = input("Enter Your name : ")
+
+# print("User enterd : ", name)
+
+# concatenation
+
+def sumthis():
+    num1 = int(input("ENter first number : "))
+    num2 = int(input("ENter second number : "))
+
+    sum = num1 + num2 
+
+    print("The sum is : ", sum)
