6dc2deb879e7f2a6c3ac9eeba51631b20f304dda
Naresh Kumar <banvro07@gmail.com>
2023-08-21 17:02:21 +0530

    Python if else if

---
+++ control_stsements.py
@@ -0,0 +1,90 @@
+# Control Statements : control the flow of code...
+# if else
+
+# age = 17
+
+# # 18 == 20
+# if age == 18:
+#     print("You are eligible for voat")
+#     print("hello")
+    
+# else:
+#     print("you are not eligible for voat")
+
+
+# if elif else:
+
+# age = 19
+# if age == 18:
+#     print("weclcome this is your first voat")
+# elif age == 19:
+#     print("nooooooooooooo")
+    
+# elif age > 18:
+#     print("You are eligibe for voat")
+
+# else:
+#     print("you are not eligible for voat")
+
+
+
+# Loops :  ittreations
+
+# a = 10
+
+# lst = [12, 20, "hlo", 120, 100, 20]
+
+# print(type(lst))
+
+# range(start, end, increment)
+
+# start = 0
+# end = n - 1
+# increment = 1
+
+
+# for i in range(1, 4, 1):
+#     print(i)
+
+# lst = [12, 20, "hlo", 120, 100, 20]
+
+
+# for x in lst:
+#     print(x)
+    # print("hellllooooooo")
+
+
+
+# for y in range(10):
+#     # print(y)
+#     if y == 5:
+#         # break
+#         # continue
+#         pass
+    
+#     print(y)
+
+
+# for i in range(12):
+#     pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
