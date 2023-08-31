f75268b32c60526b44f45ae0bdbd8f9ac5a68d0a
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-08-31 03:51:45 -0700

    Create start-oops.py

---
+++ start-oops.py
@@ -0,0 +1,123 @@
+# OOPs : Objcet oriented programming:...
+
+# 1) class
+# 2) object
+# 3) self
+# 4) constructer
+# 5) attributes
+# 6) inharitance :
+#     simple inharitance
+#     multipal inharitance
+#     mutlilavel inharitance
+#     hairaical inharitance
+#     hibrid inharitance
+
+# 7) Ploymorphiusm:
+#     duck typing
+#     mether overloading
+#     method overriding
+#     operator overloading
+
+# 8) Encapsulation:
+#     access spacifires
+#         public mambers
+#         private mambers
+#         protected mambers
+
+# 9) abstraction
+
+
+
+# Opps : 
+
+# class : class is a blueprint of an object.
+
+# object : is a instance of a class
+
+# self : self is used for refranceing
+
+# class mycls:
+#     def mthd1(self):
+#         print("i am from class myclass and methid mthd1")
+    
+#     def mthd2(self):
+#         print("i am from mthd2")
+    
+
+# obj = mycls()
+
+# obj.mthd2()
+
+# obj.mthd1()
+
+
+# constructer : use to initilizae attrebutes... __init__ , it calls automaticlly
+
+
+# class cls1:
+#     def __init__(self, a, b):
+#         # print("noooooooooooo")
+#         self.name = a
+#         self.age = b
+    
+#     def car(self, a, b):
+#         print("the sum is : ", a + b)
+    
+#     def bus(self):
+#         print("i am bus")
+    
+#     def train(self):
+#         print("this is a train..")
+
+# abc = cls1("kriss", 20)
+
+# abc.car(10,30)
+
+# abc.bus()
+
+
+
+
+
+
+
+
+
+# class cls1:
+#     def __init__(self, a, b):
+#         # print("noooooooooooo")
+#         self.name = a
+#         self.age = b
+    
+#     def car(self):
+#         print(f"my name is {self.name} and age is {self.age}")
+    
+#     def bus(self):
+#         print("i am bus")
+    
+#     def train(self):
+#         print("this is a train..")
+
+# abc = cls1("kriss", 20)
+# abc.car()
+
+# xyz = cls1("moris", 21)
+# xyz.car()
+
+
+# class
+# object
+# self
+# constructer
+# methods
+# attrebutes
+
+
+
+
+
+
+
+
+
+
